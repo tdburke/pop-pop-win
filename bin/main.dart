ddb56e200d5110ddb315c824b340c3977c838387
Thomas Burke <thomas.d.burke@gmail.com>
2013-01-21 18:18:15 -0700

    Working on server side code

--- bin/main.dart
+++ bin/main.dart
@@ -2,21 +2,20 @@ import 'dart:io';
 import 'dart:json';
 
 main() {
-  var server = new HttpServer();
+  HttpServer app = new HttpServer();
+
+  app.addRequestHandler(
+    (req) => req.method == 'GET' && req.path == '/',
+    (req, res) {
+      var file = new File('../web/index.html');
+      var stream = file.openInputStream();
+      stream.pipe(res.outputStream);
+    }
+  );
+  
   var port = int.parse(Platform.environment['PORT']);
-  server.listen('0.0.0.0', port);
-  print('Server started on port: ${port}');
 
-  server.defaultRequestHandler = (HttpRequest request, HttpResponse response) {
-
-    var resp = JSON.stringify({
-      'Dart on Heroku BURKE': true,
-      'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
-      'Environment': Platform.environment}
-    );
-
-    response.headers.set(HttpHeaders.CONTENT_TYPE, 'application/json');
-    response.outputStream.writeString(resp);
-    response.outputStream.close();
-  };
+  app.listen('0.0.0.0', port);
 }
+
+
